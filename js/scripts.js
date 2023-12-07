4d500bbbd914469dc39aeb661c3a269987b1ede9
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2023-12-07 18:12:06 +0100

    Discount 40%

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,12 @@ console.log('age', age, typeof age);
 const ageNumber = parseInt(age);
 console.log('ageNumber', ageNumber, typeof ageNumber);
 
+
 const price = (km * 0.21);
 console.log('price', price, typeof price);
+
+const priceContainer = document.getElementById('price-container');
+console.log('priceContainer', priceContainer, typeof priceContainer);
 
 if (ageNumber < 18) {
     const discount = (price / 100) * 20;
@@ -27,4 +31,20 @@ if (ageNumber < 18) {
 
     const discountedTicket = (price - discount);
     console.log('discountedTicket', discountedTicket, typeof discountedTicket);
+    
+    priceContainer.innerHTML = discountedTicket;
+}
+
+else if (ageNumber > 65) {
+    const discount = (price / 100) * 40;
+    console.log('discount', discount, typeof discount);
+
+    const discountedTicket = (price - discount);
+    console.log('discountedTicket', discountedTicket, typeof discountedTicket);
+    
+    priceContainer.innerHTML = discountedTicket;
+}
+
+else if (ageNumber > 18, ageNumber < 65) {
+    priceContainer.innerHTML = price;
 }
